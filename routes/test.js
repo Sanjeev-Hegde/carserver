c61a7123aae0dbeaa4f22c7eac97e9d62cc102ed
sanjeev-hegde <sanjeevh.93@gmail.com>
2017-04-27 22:19:44 +0530

    Added apis to controll 2 motors

--- routes/test.js
+++ routes/test.js
@@ -37,4 +37,161 @@ function write() {
     res.send({"name":"Monster Truck","controllType":"Wify"});
 });
 
+/**
+ * @swagger
+ * /truck_api/test/move_forward:
+ *   get:
+ *     tags:
+ *       - Monster Truck
+ *     description: Moves the car forward'
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Moves the car forward
+ *
+ */
+router.get('/move_forward', function(req, res, next) {
+
+    gpio.setup(7, gpio.DIR_OUT, function write() {
+        gpio.write(7, true, function(err) {
+            if (err) throw err;
+            console.log('Moving Forward');
+        });
+    });
+    res.send({"action":"Moving Forward"});
+});
+/**
+ * @swagger
+ * /truck_api/test/move_backward:
+ *   get:
+ *     tags:
+ *       - Monster Truck
+ *     description: Moves the car backward'
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Moves the car backward
+ *
+ */
+router.get('/move_backward', function(req, res, next) {
+
+    gpio.setup(8, gpio.DIR_OUT, function write() {
+        gpio.write(8, true, function(err) {
+            if (err) throw err;
+            console.log('Moving Backward');
+        });
+    });
+    res.send({"action":"Moving Backward"});
+});
+/**
+ * @swagger
+ * /truck_api/test/stop_vertical_movement:
+ *   get:
+ *     tags:
+ *       - Monster Truck
+ *     description: Stops the vertical movement of car'
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Stops the vertical movement of car
+ *
+ */
+router.get('/stop_vertical_movement', function(req, res, next) {
+
+    gpio.setup(7, gpio.DIR_OUT, function write() {
+        gpio.write(7, false, function(err) {
+            if (err) throw err;
+        });
+    });
+    gpio.setup(8, gpio.DIR_OUT, function write() {
+        gpio.write(8, false, function(err) {
+            if (err) throw err;
+        });
+    });
+    console.log('Vertical Movement Stopped');
+    res.send({"action":"Vertical Movement Stopped"});
+});
+
+
+/**
+ * @swagger
+ * /truck_api/test/move_right:
+ *   get:
+ *     tags:
+ *       - Monster Truck
+ *     description: Moves the car right'
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Moves the car right
+ *
+ */
+router.get('/move_right', function(req, res, next) {
+
+    gpio.setup(9, gpio.DIR_OUT, function write() {
+        gpio.write(9, true, function(err) {
+            if (err) throw err;
+            console.log('Moving Right');
+        });
+    });
+    res.send({"action":"Moving Right"});
+});
+/**
+ * @swagger
+ * /truck_api/test/move_left:
+ *   get:
+ *     tags:
+ *       - Monster Truck
+ *     description: Moves the car left'
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Moves the car left
+ *
+ */
+router.get('/move_left', function(req, res, next) {
+
+    gpio.setup(10, gpio.DIR_OUT, function write() {
+        gpio.write(10, true, function(err) {
+            if (err) throw err;
+            console.log('Moving Left');
+        });
+    });
+    res.send({"action":"Moving Left"});
+});
+/**
+ * @swagger
+ * /truck_api/test/stop_horizontal_movement:
+ *   get:
+ *     tags:
+ *       - Monster Truck
+ *     description: Stops the horizontal movement of car'
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: Stops the horizontal movement of car
+ *
+ */
+router.get('/stop_horizontal_movement', function(req, res, next) {
+
+    gpio.setup(9, gpio.DIR_OUT, function write() {
+        gpio.write(9, false, function(err) {
+            if (err) throw err;
+        });
+    });
+    gpio.setup(10, gpio.DIR_OUT, function write() {
+        gpio.write(10, false, function(err) {
+            if (err) throw err;
+        });
+    });
+    console.log('Horizontal Movement Stopped');
+    res.send({"action":"Horizontal Movement Stopped"});
+});
+
 module.exports = router;
